0835c11523fd58a652442c03127f36159549785d
allab17 <labella@lafayette.edu>
2022-10-22 12:46:45 -0400

    lab05 final commit

--- lab05/Code/ts_lcd.c
+++ lab05/Code/ts_lcd.c
@@ -5,8 +5,8 @@
 
 const int32_t x_1 = 0;
 const int32_t y_1 = 0;
-const int32_t x_2 = 319;
-const int32_t y_2 = 239;
+const int32_t x_2 = 239;
+const int32_t y_2 = 319;
 
 const int32_t x1 = 160;
 const int32_t y1 = 108;
@@ -23,12 +23,22 @@ uint8_t ts_lcd_get_ts(uint16_t *x, uint16_t *y) {
     getPoint(&p);
     *x = (((p.x-x1)*(x_2-x_1))/(x2-x1)) + x_1;
     *y = (((p.y-y1)*(y_2-y_1))/(y2-y1)) + y_1;
-    if (p.z > 75) return 1;
+    if (p.z > 30) return 1;
     else return 0;
 }
 
 void ts_lcd_init() {
     
+}
+
+uint8_t pressed() {
+    struct TSPoint p;
+    p.x = 0;
+    p.y = 0;
+    p.z = 0;
+    getPoint(&p);
+    if (p.z > 30) return 1;
+    else return 0;
 }
 
 
